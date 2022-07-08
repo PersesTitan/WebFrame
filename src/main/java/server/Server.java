0a576473a1ddb5d3c71ea70c8e22f4dcf3cedf34
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-08 12:38:30 +0900

    생성하는 로직 추가

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,50 +1,20 @@
 package server;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import items.Color;
+import items.Temporary;
 
-public class Server {
-
-    private final int port;
-
-    //set port
-    public Server() {
-        this.port = 9090;
-    }
-
-    public Server(int port) {
-        this.port = port;
-    }
+public class Server implements Temporary {
 
     public void connectServer() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            Socket connectSocket = serverSocket.accept();
-
-            InputStream inputServer = connectSocket.getInputStream();
-            OutputStream outputServer = connectSocket.getOutputStream();
-
-            Scanner scanner = new Scanner(inputServer, StandardCharsets.UTF_8);
-            PrintWriter serverPrint = new PrintWriter(new OutputStreamWriter(outputServer, StandardCharsets.UTF_8), true);
-
-            serverPrint.println("[OTLanguage Server] Start");
-            serverPrint.println(String.format("PORT: %s", port));
-
-            boolean done = false;
-
-            while (!done && scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                serverPrint.println("서버 출력: " + line);
-
-                if (line.strip().equalsIgnoreCase("exit")) {
-                    done = true;
-                }
+        HttpServerManager httpServerManager = new HttpServerManager();
+        try {
+            httpServerManager.start();
+            if (path.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
+            else {
+                while (true) {}
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } finally {
+            httpServerManager.stop();
         }
     }
 }
