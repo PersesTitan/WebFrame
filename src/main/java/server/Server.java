ce2ae3c663caf91b32570100d29faa039a1d0a7f
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-08 14:31:05 +0900

    server 추가

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,11 +5,24 @@ import items.Temporary;
 
 public class Server implements Temporary {
 
-    public void connectServer() {
+    public Server() {
         HttpServerManager httpServerManager = new HttpServerManager();
         try {
             httpServerManager.start();
-            if (path.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
+            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
+            else {
+                while (true) {}
+            }
+        } finally {
+            httpServerManager.stop();
+        }
+    }
+
+    public Server(int port) {
+        HttpServerManager httpServerManager = new HttpServerManager(port);
+        try {
+            httpServerManager.start();
+            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
             else {
                 while (true) {}
             }
